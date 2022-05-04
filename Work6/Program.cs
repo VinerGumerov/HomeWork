73ad133d1326894aca0540587936bf8c6a258a71
Winner <V-gumerov@bk.ru>
2022-05-04 23:24:31 +0500

    The end 2 homework6

---
+++ Work6/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+//Задача 41:
+//Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+Console.Write("Enter numbers through whitespace: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Числа > 0: {count}");
+
+
+//Задача 42:
+//Напишите программу, 
+//которая будет преобразовывать десятичное число в двоичное.
+
+string DecimalNumbers(int num)
+{
+    string result = String.Empty;
+    while (num > 0)
+    {
+        result += Convert.ToString((num % 2));
+        num /= 2;
+    }
+    result = new string (result.Reverse().ToArray());
+    return  result;
+}
+Console.Write("Input number to convert in DecimalNumbers: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"In binary system = {DecimalNumbers(num2)}");
+
+*/
