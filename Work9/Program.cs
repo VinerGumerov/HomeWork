43045d31d128a178d5a82d6d0c8334e5a518a038
Winner <V-gumerov@bk.ru>
2022-05-24 00:55:45 +0500

    The end homework9

---
+++ Work9/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 66: 
+//Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+/*
+int SumNum(int M, int N)
+{
+    if (M < N)
+    {
+        M +=1;
+        return M + SumNum(M,N);
+    }
+    else return 1;
+}
+Console.WriteLine(SumNum(1,15));
+*/
+
+//Задача 67: 
+//Напишите программу, 
+//которая будет принимать на вход число и возвращать сумму его цифр.
+
+int NaturalNums(int n)
+{
+    if (n >= 1)
+    {
+        n -= 1;
+        return (n+1) + NaturalNums(n);
+    }
+    else return 0;
+}
+Console.WriteLine(NaturalNums(9));
+
+
