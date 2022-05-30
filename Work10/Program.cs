bc35ef158388ad612443f4e618833bf664e82b3c
Winner <V-gumerov@bk.ru>
2022-05-30 21:44:24 +0500

    The end homework10

---
+++ Work10/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 1: 
+//Задайте массив строк. Напишите программу.
+//Cчитает кол-во слов в массиве, начинающихся на гласную букву.
+
+string[] name =  { "qwe", "wer", "ert", "rty", "tyu"};
+int CountVowel(string[] words)
+{
+    string vowels = "aeiouy";
+    int count = 0;
+    for (int i = 0; i < words.Length; i++)
+    {
+        if(words[i][0] == vowels) count++;
+    }
+    return count;
+}
+Console.WriteLine(CountVowel(name));
+
+
+
